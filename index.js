621add7f22460fcab92a8a827bd44695942b4800
Marcus Spiegel <spiegel@uscreen.de>
2019-08-09 14:16:30 +0200

    added sensible, oas, autoload

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const path = require('path')
 const readPkgUp = require('read-pkg-up')
 const fp = require('fastify-plugin')
 const helmet = require('fastify-helmet')
+const sensible = require('fastify-sensible')
+const oas = require('fastify-oas')
+const autoload = require('fastify-autoload')
 
-module.exports = fp(async (fastify, opts, next) => {
+module.exports = fp(async (fastify, config, next) => {
+  console.log(config)
+
   /**
    * read package information
    */
@@ -24,6 +29,16 @@ module.exports = fp(async (fastify, opts, next) => {
   }
 
   /**
+   * add config
+   */
+  fastify.decorate('config', config)
+
+  /**
+   * add OpenAPI docs (v3.0 aka swagger)
+   */
+  fastify.register(oas, config.swagger)
+
+  /**
    * add helmet (http security headers)
    */
   fastify.register(helmet, {
@@ -31,6 +46,21 @@ module.exports = fp(async (fastify, opts, next) => {
       setTo: `${fastify.name} ${fastify.version}`
     }
   })
+
+  /**
+   * add sensible defaults
+   */
+  fastify.register(sensible)
+
+  /**
+   * autoload plugins, services, etc
+   */
+  for (const dir of config.autoloads) {
+    fastify.register(autoload, {
+      dir,
+      options: config
+    })
+  }
 
   /**
    * post-treatment
