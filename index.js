b3f3b73d118fd93ef28dd13f14bb38000a5fef23
Marcus Spiegel <spiegel@uscreen.de>
2019-08-09 13:23:52 +0200

    reading package, decorating fastify

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 'use strict'
 
-console.log('hello node');
+const path = require('path')
+const readPkgUp = require('read-pkg-up')
+const fp = require('fastify-plugin')
+
+module.exports = fp(async (fastify, opts, next) => {
+  /**
+   * read package information
+   */
+  const pack = readPkgUp.sync()
+  const pkg = {
+    name: pack.package.name,
+    version: pack.package.version,
+    root: path.dirname(pack.path)
+  }
+
+  /**
+   * add package information
+   */
+  for (const [k, v] of Object.entries(pkg)) {
+    fastify.decorate(k, v)
+  }
+
+  /**
+   * post-treatment
+   */
+  fastify.ready(err => {
+    if (err) throw err
+    fastify.log.debug(
+      `${fastify.name} (${fastify.version}) ready. pwd: ${fastify.root}`
+    )
+  })
+
+  next()
+})
