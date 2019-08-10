2529ba2b93c3e8dab24cafe73c8c1973ec9b2500
Marcus Spiegel <spiegel@uscreen.de>
2019-08-10 20:12:21 +0200

    split tests

--- test/app.custom.test.js
+++ test/app.custom.test.js
@@ -1,8 +1,14 @@
 const tap = require('tap')
 const { build } = require('./helper')
 
-tap.test('basic bootstrapping without custom config', t => {
-  const fastify = build(t)
+tap.test('basic bootstrapping with some custom config and overwrites', t => {
+  const fastify = build(t, {
+    autoloads: ['one', 'two', 'three'],
+    hazCustomConfig: true,
+    swagger: {
+      routePrefix: '/docs'
+    }
+  })
 
   fastify.ready(err => {
     t.test('should not throw any error', t => {
@@ -10,25 +16,18 @@ tap.test('basic bootstrapping without custom config', t => {
       t.end()
     })
 
-    t.test('should expose some package.json details', t => {
-      t.ok(fastify.name)
-      t.ok(fastify.version)
-      t.ok(fastify.root)
-      t.end()
-    })
-
     t.test('should expose it`s config', t => {
       t.type(fastify.config, 'object')
-      t.same(fastify.config.autoloads, [])
-      t.same(fastify.config.swagger, { exposeRoute: true })
+      t.same(fastify.config.autoloads, ['one', 'two', 'three'])
+      t.same(fastify.config.hazCustomConfig, true)
       t.end()
     })
 
-    t.test('should provide openapi json url', t => {
+    t.test('should provide openapi json on custom url', t => {
       fastify.inject(
         {
           method: 'GET',
-          url: '/documentation/json'
+          url: '/docs/json'
         },
         (e, response) => {
           const body = JSON.parse(response.body)
