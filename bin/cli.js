6d3ef9f7d0e80fafd62bfc8eacc3ca37944c5e78
Marcus Spiegel <spiegel@uscreen.de>
2019-08-15 18:38:52 +0200

    add linting packages

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,71 @@
 #!/usr/bin/env node
 
-console.error(
-  '`fastify-app` cli is not yet available... wait for version 0.2.0'
-)
-process.exit(1)
+const path = require('path')
+const cli = require('commander')
+const readPkgUp = require('read-pkg-up')
+const { spawn } = require('child_process')
+
+/**
+ * read package information
+ */
+const pack = readPkgUp.sync()
+const root = path.dirname(pack.path)
+
+const installPackages = () =>
+  new Promise((resolve, reject) => {
+    const yarn = spawn(
+      'yarn',
+      [
+        'add',
+        'eslint',
+        'eslint-config-prettier',
+        'eslint-config-standard',
+        'eslint-plugin-import',
+        'eslint-plugin-node',
+        'eslint-plugin-prettier',
+        'eslint-plugin-promise',
+        'eslint-plugin-standard',
+        'lint-staged',
+        'prettier',
+        'yorkie',
+        '-D'
+      ],
+      {
+        cwd: root
+      }
+    )
+    yarn.stdout.on('data', data => process.stdout.write(data))
+    yarn.stderr.on('data', data => process.stderr.write(data))
+    yarn.on('close', code => {
+      if (code === 0) return resolve(code)
+      reject(code)
+    })
+  })
+
+/**
+ * define a command
+ */
+cli
+  .command('init')
+  .description('Interactivly scaffold a new project')
+  .action(async () => {
+    try {
+      await installPackages()
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+/**
+ * read args
+ */
+cli.parse(process.argv)
+
+/**
+ * output help as default
+ */
+if (!process.argv.slice(2).length) {
+  cli.help()
+}
+
+// process.exit(1)
